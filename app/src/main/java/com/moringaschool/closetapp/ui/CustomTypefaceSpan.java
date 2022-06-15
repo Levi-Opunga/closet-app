288c00d963605a98aa3eaa42481a8a85c757e767
Levi-Opunga <leviopunga@gmail.com>
2022-06-15 04:59:26 +0300

    Added font as asset

---
+++ app/src/main/java/com/moringaschool/closetapp/ui/CustomTypefaceSpan.java
@@ -0,0 +1,9 @@
+package com.moringaschool.closetapp.ui;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+public class CustomTypefaceSpan {
+    public CustomTypefaceSpan(String s, Typeface font, Context mContext) {
+    }
+}
